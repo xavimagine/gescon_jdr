a0a92eef58ae15b7b181649296cfaeb5677ba592
xavimagine <sr.xavier5@gmail.com>
2026-05-06 18:41:00 +0200

    .2

--- src/jdr/controller/CocheController.js
+++ src/jdr/controller/CocheController.js
@@ -14,7 +14,7 @@ class CocheController {
             const coches = await cocheService.consultarCochesPorMarca(marca);
             return res.status(200).json(coches);
         } catch (error) {
-            console.logg(error);
+            console.log(error);
             return res.status(500).json({ error: error.message });
         }
     }
