8577bb80a9c0f463f0ba156f9eca18898bc27ebb
xavimagine <sr.xavier5@gmail.com>
2026-05-06 14:59:02 +0200

    cambio dotnet

--- src/jdr/index.js
+++ src/jdr/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-require("dotenv").config();
+require("dotenv").config({ path: path.resolve(__dirname, "../../.env") });
 const cocheRoutes = require("./routes/cocheRoutes");
 
 const app = express();
