2d2e071a7496ffa697ec87212b21f5334612b275
xavimagine <sr.xavier5@gmail.com>
2026-05-06 19:05:20 +0200

    db ip

--- src/jdr/config/db.js
+++ src/jdr/config/db.js
@@ -1,7 +1,7 @@
 const mysql2 = require("mysql2/promise");
 
 const pool = mysql2.createPool({
-    host: process.env.DB_HOST || "localhost",
+    host: process.env.DB_HOST || "52.55.93.91",
     user: process.env.DB_USER || "root",
     password: process.env.DB_PASSWORD || "root1234",
     database: process.env.DB_NAME || "concesionario_jdr",
