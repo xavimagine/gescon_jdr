0c4cd3d7f93877da5e80e9569f759edd3523e7d2
xavimagine <sr.xavier5@gmail.com>
2026-05-06 17:37:48 +0200

    x

--- src/jdr/config/db.js
+++ src/jdr/config/db.js
@@ -1,14 +1,17 @@
 const mysql2 = require("mysql2/promise");
-
-const pool = mysql2.createPool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 3306,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
-});
+try {
+    const pool = mysql2.createPool({
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT || 3306,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        waitForConnections: true,
+        connectionLimit: 10,
+        queueLimit: 0,
+    });
+} catch (error) {
+    console.log(error);
+}
 
 module.exports = pool;
