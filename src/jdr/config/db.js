5adbd9c214744ec961af7a127832c838f50afd68
xavimagine <sr.xavier5@gmail.com>
2026-05-06 15:05:07 +0200

    final?

--- src/jdr/config/db.js
+++ src/jdr/config/db.js
@@ -3,9 +3,9 @@ const mysql2 = require("mysql2/promise");
 const pool = mysql2.createPool({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT || 3306,
-    user: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "12345",
-    database: process.env.DB_NAME || "concesionario_jdr",
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
